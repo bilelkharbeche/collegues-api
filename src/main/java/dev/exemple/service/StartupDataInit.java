0910f0d53a479b6f2aaa93f2255e471db80e49c4
Bilel KHARBECHE <bilelkharbeche71@gmail.com>
2019-09-25 15:06:30 +0200

    url modif

--- src/main/java/dev/exemple/service/StartupDataInit.java
+++ src/main/java/dev/exemple/service/StartupDataInit.java
@@ -29,8 +29,9 @@ public class StartupDataInit {
 				LocalDate.of(1997, 05, 06), "https://www.bing.com/th?id=OIP.4bw-bn6TArZf54StdtBlSwHaFj&pid=Api&rs=1",
 				passwordEncoder.encode("pass1"), Arrays.asList("ROLE_ADMIN", "ROLE_USER")));
 		collegueRepository.save(new Collegue(UUID.randomUUID().toString(), "KHARBECHE2", "Bilel2", "test@hotmail.fr2",
-				LocalDate.of(1997, 05, 06), "URLDEMALADE2", passwordEncoder.encode("pass2"),
-				Arrays.asList("ROLE_USER")));
+				LocalDate.of(1997, 05, 06),
+				"https://i.pinimg.com/originals/44/9c/17/449c175588d56442fd162990ac428759.gif",
+				passwordEncoder.encode("pass2"), Arrays.asList("ROLE_USER")));
 		collegueRepository.save(new Collegue(UUID.randomUUID().toString(), "KHARBECHE3", "Bilel3", "test@hotmail.fr3",
 				LocalDate.of(1997, 05, 06), "URLDEMALADE3", passwordEncoder.encode("pass3"),
 				Arrays.asList("ROLE_USER")));
